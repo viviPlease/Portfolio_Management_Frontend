c3eae3725828ca93598415e06e76379f99ca11b7
JackS2019 <964800156@qq.com>
2023-08-22 16:45:35 +0800

    Login Implemented

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NgxLoginComponent } from './login/login.component'; 
 import { NbAuthComponent } from '@nebular/auth';
-import { NgxRegisterComponent } from './register/register.component'; 
+import { NgxRegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   // .. here goes our components routes
